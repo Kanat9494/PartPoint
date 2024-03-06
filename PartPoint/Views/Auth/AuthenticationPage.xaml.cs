b4e8299fb90fb813ac0b59cc54752b3ed391cb3f
kana2022 <kkanat94@gmail.com>
2024-03-06 18:59:28 +0600

    1

--- PartPoint/Views/Auth/AuthenticationPage.xaml.cs
+++ PartPoint/Views/Auth/AuthenticationPage.xaml.cs
@@ -8,7 +8,10 @@ public partial class AuthenticationPage : ContentPage
 
         _authService = authService;
 
-        //activityBorder.BackgroundColor = Color.FromRgba(0, 0, 0, 160);
+        firstTab.BackgroundColor = Color.FromArgb("#00cc00");
+        secondTab.BackgroundColor = Colors.Transparent;
+        firstTabTitle.TextColor = Color.FromArgb("#00cc00");
+        secondTabTitle.TextColor = Colors.Black;
 
         BindingContext = new AuthenticationViewModel(authService);
     }
@@ -20,5 +23,25 @@ public partial class AuthenticationPage : ContentPage
         base.OnNavigatedTo(args);
 
         Task.Run(_authService.IsUserAuthenticated).Wait();
+    }
+
+    void OnPositionChanged(object sender, PositionChangedEventArgs e)
+    {
+        int currentItemPosition = e.CurrentPosition;
+
+        if (currentItemPosition == 0)
+        {
+            firstTab.Background = Color.FromArgb("00cc00");
+            secondTab.Background = Colors.Transparent;
+            firstTabTitle.TextColor = Color.FromArgb("00cc00");
+            secondTabTitle.TextColor = Colors.Black;
+        }
+        else
+        {
+            firstTab.Background = Colors.Transparent;
+            secondTab.Background = Color.FromArgb("00cc00");
+            firstTabTitle.TextColor = Colors.Black;
+            secondTabTitle.TextColor = Color.FromArgb("00cc00");
+        }
     }
 }
---
+++ PartPoint/Views/TemplateSelectors/AuthUISelector.cs
@@ -0,0 +1,16 @@
+﻿namespace PartPoint.Views.TemplateSelectors;
+
+public class AuthUISelector : DataTemplateSelector
+{
+    public DataTemplate SignInTemplate { get; set; }
+    public DataTemplate SignOutTemplate { get; set;}
+
+    protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+    {
+        var title = item as string;
+        if (title!.Equals("Вход"))
+            return SignInTemplate;
+
+        return SignOutTemplate;
+    }
+}
