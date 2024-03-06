cae86568720a12762dbba7434c785cf929b8a586
kana2022 <kkanat94@gmail.com>
2024-03-06 12:45:38 +0600

    Add project files.

---
+++ PartPoint/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PartPoint
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PartPoint/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PartPoint
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
