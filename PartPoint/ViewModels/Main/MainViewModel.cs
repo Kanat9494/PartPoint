e9fea3fa97305f7a83f1bcb0d1d6cc84e79790f0
Kanat9494 <kkanat94@gmail.com>
2024-03-09 20:05:37 +0600

    1

--- PartPoint/ViewModels/Main/MainViewModel.cs
+++ PartPoint/ViewModels/Main/MainViewModel.cs
@@ -6,7 +6,17 @@ public class MainViewModel : BaseViewModel
     {
         _authService = authService;
         _authService.IsUserAuthenticated();
+
+        Task.Run(InitializeContent);
     }
 
     private readonly IAuthService _authService;
+
+    private async Task InitializeContent()
+    {
+        IsBusy = true;
+        await Task.Delay(3000);
+
+        IsBusy = false;
+    }
 }
